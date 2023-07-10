1ea8276157b7aea92fcef91b239c4da2dba0f018
SmoonliX <ananas1.19@mail.ru>
2023-07-10 18:49:45 +0300

    homework4

---
+++ MyVector/src/main.rs
@@ -0,0 +1,89 @@
+struct MyVector<T: Copy> {
+    data: [(Option<T>, usize); 10],
+    length: usize,
+}
+
+trait TraitName<T: Copy> {
+    fn new() -> Self;
+    fn with_capacity(_capacity: usize) -> Self;
+    fn push(&mut self, element: T);
+    fn pop(&mut self) -> Option<T>;
+    fn remove(&mut self, index: usize) -> Option<T>;
+    fn get(&self, index: usize) -> Option<&T>;
+    fn resize(&mut self, _new_size: usize);
+}
+
+impl<T: Copy> TraitName<T> for MyVector<T> {
+    fn new() -> Self {
+        Self {
+            data: [(None, 0); 10],
+            length: 0,
+        }
+    }
+
+    fn with_capacity(_capacity: usize) -> Self {
+        Self {
+            data: [(None, 0); 10],
+            length: 0,
+        }
+    }
+
+    fn push(&mut self, element: T) {
+        if self.length < self.data.len() {
+            self.data[self.length] = (Some(element), 0);
+            self.length += 1;
+        } else {
+            self.resize(self.data.len() * 2);
+            self.push(element); 
+        }
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        if self.length > 0 {
+            self.length -= 1;
+            self.data[self.length].0.take()
+        } else {
+            None
+        }
+    }
+
+    fn remove(&mut self, index: usize) -> Option<T> {
+        if index < self.length {
+            let removed = self.data[index].0.take();
+            for i in index..self.length - 1 {
+                self.data[i].0 = self.data[i + 1].0.take();
+            }
+            self.length -= 1;
+            removed
+        } else {
+            None
+        }
+    }
+
+    fn get(&self, index: usize) -> Option<&T> {
+        if index < self.length {
+            self.data[index].0.as_ref()
+        } else {
+            None
+        }
+    }
+
+    fn resize(&mut self, _new_size: usize) {
+        let mut new_data: [(Option<T>, usize); 10] = [(None, 0); 10];
+        for i in 0..self.length {
+            new_data[i].0 = self.data[i].0.take();
+        }
+        self.data = new_data;
+    }
+}
+
+fn main() {
+    let mut vector: MyVector<i32> = MyVector::new();
+    vector.push(1);
+    vector.push(2);
+    vector.push(3);
+    println!("{:?}", vector.pop()); 
+    println!("{:?}", vector.get(1)); 
+    vector.remove(0);
+    println!("{:?}", vector.get(0)); 
+}
